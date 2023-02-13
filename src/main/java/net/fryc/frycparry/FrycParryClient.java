e5fbbfa427604b201e044427b9bef778c1e148a9
Xires87 <frycmod@gmail.com>
2023-02-13 18:28:44 +0100

    fixed crash when skeleton picks up a shield

--- src/main/java/net/fryc/frycparry/FrycParryClient.java
+++ src/main/java/net/fryc/frycparry/FrycParryClient.java
@@ -3,7 +3,6 @@ package net.fryc.frycparry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fryc.frycparry.util.ParryHelper;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
@@ -12,11 +11,11 @@ public class FrycParryClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ModelPredicateProviderRegistry.register(new Identifier("parry"), (stack, world, entity, seed) -> {
-            return entity != null && (entity.getOffHandStack().isEmpty() || ParryHelper.checkDualWieldingWeapons((PlayerEntity) entity) || ParryHelper.checkDualWieldingItems((PlayerEntity) entity)) && entity.isUsingItem() && entity.getActiveItem().getItem() instanceof SwordItem ? 1.0F : 0.0F;
+            return entity != null && (entity.getOffHandStack().isEmpty() || ParryHelper.checkDualWieldingWeapons(entity) || ParryHelper.checkDualWieldingItems(entity)) && entity.isUsingItem() && entity.getActiveItem().getItem() instanceof SwordItem ? 1.0F : 0.0F;
         });
 
         ModelPredicateProviderRegistry.register(new Identifier("axeparry"), (stack, world, entity, seed) -> {
-            return entity != null && (entity.getOffHandStack().isEmpty() || ParryHelper.checkDualWieldingWeapons((PlayerEntity) entity) || ParryHelper.checkDualWieldingItems((PlayerEntity) entity)) && entity.isUsingItem() && entity.getActiveItem().getItem() instanceof AxeItem ? 1.0F : 0.0F;
+            return entity != null && (entity.getOffHandStack().isEmpty() || ParryHelper.checkDualWieldingWeapons(entity) || ParryHelper.checkDualWieldingItems(entity)) && entity.isUsingItem() && entity.getActiveItem().getItem() instanceof AxeItem ? 1.0F : 0.0F;
         });
 
     }
--- src/main/java/net/fryc/frycparry/util/ParryHelper.java
+++ src/main/java/net/fryc/frycparry/util/ParryHelper.java
@@ -1,16 +1,16 @@
 package net.fryc.frycparry.util;
 
 import net.fryc.frycparry.FrycParry;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.*;
 
 public class ParryHelper {
 
-    public static boolean checkDualWieldingWeapons(PlayerEntity user){
+    public static boolean checkDualWieldingWeapons(LivingEntity user){
         return FrycParry.config.enableBlockingWhenDualWielding > 0 && (user.getOffHandStack().getItem() instanceof SwordItem || user.getOffHandStack().getItem() instanceof AxeItem);
     }
 
-    public static boolean checkDualWieldingItems(PlayerEntity user){
+    public static boolean checkDualWieldingItems(LivingEntity user){
         return FrycParry.config.enableBlockingWhenDualWielding > 1 && !(user.getOffHandStack().getItem() instanceof ShieldItem) && !(user.getOffHandStack().getItem() instanceof BowItem) && !(user.getOffHandStack().getItem() instanceof CrossbowItem);
     }
 }
