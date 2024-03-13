190ebc73e687c0531e211872512517c819a0093d
Xires87 <frycmod@gmail.com>
2024-03-13 19:46:57 +0100

    replaced WrapOperation from ItemRendererMixin.java with Inject

--- src/main/java/net/fryc/frycparry/mixin/client/ItemRendererMixin.java
+++ src/main/java/net/fryc/frycparry/mixin/client/ItemRendererMixin.java
@@ -1,42 +1,33 @@
 package net.fryc.frycparry.mixin.client;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.fryc.frycparry.util.client.ItemRendererHelper;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
-import net.minecraft.client.render.model.json.Transformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemRenderer.class)
 abstract class ItemRendererMixin {
 
-    @WrapOperation(
-            method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;" +
-                    "ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;" +
-                    "IILnet/minecraft/client/render/model/BakedModel;)V",
-            at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/render/model/json/Transformation;apply(ZLnet/minecraft/client/util/math/MatrixStack;)V")
-    )
-    private void applyParryModelTransformation(Transformation transformation, boolean leftHanded, MatrixStack matrices, Operation<Void> original,
-                      ItemStack stack, ModelTransformationMode renderMode, boolean leftHandedv2, MatrixStack matricesv2,
-                      VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model) {
-
+    @Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;" +
+            "ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;" +
+            "IILnet/minecraft/client/render/model/BakedModel;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;translate(FFF)V"))
+    private void applyParryModelTransformation(ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices,
+                          VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo info) {
 
         if(renderMode == ModelTransformationMode.FIRST_PERSON_RIGHT_HAND && ItemRendererHelper.shouldApplyParryTransform && stack.getItem() instanceof ToolItem){
-            ItemRendererHelper.applyParryTransform(matrices, transformation);
-        }
-        else {
-            original.call(transformation, leftHanded, matrices);
+            matrices.pop();
+            matrices.push();
+            ItemRendererHelper.applyParryTransform(matrices, model.getTransformation().getTransformation(renderMode));
         }
     }
-
-
 
 }
