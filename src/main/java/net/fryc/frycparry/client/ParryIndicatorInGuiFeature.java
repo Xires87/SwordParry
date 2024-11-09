326a2163f0f9ea02e63360c13a0a130f68ffdcae
execsuroot <execsuroot@gmail.com>
2024-11-09 22:04:54 +0200

    chore: Reposition parry indicator

--- src/main/java/net/fryc/frycparry/client/ParryIndicatorInGuiFeature.java
+++ src/main/java/net/fryc/frycparry/client/ParryIndicatorInGuiFeature.java
@@ -13,7 +13,7 @@ public class ParryIndicatorInGuiFeature {
     public static void render(DrawContext context, int screenWidth, int screenHeight, float progress) {
         RenderSystem.blendFuncSeparate(GlStateManager.SrcFactor.ONE_MINUS_DST_COLOR, GlStateManager.DstFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO);
         int posX = screenWidth / 2 - 8;
-        int posY = screenHeight / 2 - 7 + 24;
+        int posY = screenHeight / 2 - 7 + 55;
         int textureHeight = (int) (progress * 15);
         context.drawTexture(ICONS_TEXTURE, posX, posY, 54, 0, 16, 14, 256, 256);
         context.drawTexture(ICONS_TEXTURE, posX, posY + 14 - textureHeight, 70, 14 - textureHeight, 16, textureHeight, 256, 256);
