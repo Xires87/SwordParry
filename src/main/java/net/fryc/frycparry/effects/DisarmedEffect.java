d9b8653bc95d492b5ac7028110fad376e55b2aa3
Xires87 <frycmod@gmail.com>
2025-08-20 10:26:30 +0200

    disarmed sets cooldown no matter what item was used

--- src/main/java/net/fryc/frycparry/effects/DisarmedEffect.java
+++ src/main/java/net/fryc/frycparry/effects/DisarmedEffect.java
@@ -1,6 +1,5 @@
 package net.fryc.frycparry.effects;
 
-import net.fryc.frycparry.util.ParryHelper;
 import net.fryc.frycparry.util.interfaces.TargetingMob;
 import net.minecraft.command.argument.EntityAnchorArgumentType;
 import net.minecraft.entity.LivingEntity;
@@ -8,10 +7,7 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ShieldItem;
-import net.minecraft.world.World;
 
 public class DisarmedEffect extends StatusEffect {
     protected DisarmedEffect(StatusEffectCategory statusEffectCategory, int color) {
@@ -25,31 +21,22 @@ public class DisarmedEffect extends StatusEffect {
             ItemStack offItem = player.getOffHandStack();
 
             if(!offItem.isEmpty()){
-                if(isShieldOrBowOrTool(entity.getWorld(), offItem)){
-                    player.getItemCooldownManager().set(offItem.getItem(), duration);
-                }
+                player.getItemCooldownManager().set(offItem.getItem(), duration);
             }
 
             if(!mainItem.isEmpty()){
-                if(isShieldOrBowOrTool(entity.getWorld(), mainItem)){
-                    player.getItemCooldownManager().set(mainItem.getItem(), duration);
-                }
+                player.getItemCooldownManager().set(mainItem.getItem(), duration);
             }
         }
         else if(entity instanceof MobEntity mob){
             mob.setAttacking(false);
             ((TargetingMob) mob).setLastTarget(mob.getTarget());
             mob.setTarget(null);
-            if(mob.getTarget() != null){
-                mob.lookAt(EntityAnchorArgumentType.EntityAnchor.FEET, mob.getTarget().getPos());
+            if(((TargetingMob) mob).getLastTarget() != null){
+                mob.lookAt(EntityAnchorArgumentType.EntityAnchor.FEET, ((TargetingMob) mob).getLastTarget().getPos());
             }
         }
         super.onApplied(entity, amplifier);
-    }
-
-    private static boolean isShieldOrBowOrTool(World world, ItemStack stack){
-        Item item = stack.getItem();
-        return item instanceof ShieldItem || ParryHelper.hasAttackSpeedAttribute(stack) || !ParryHelper.isItemParryDisabledWithConfig(world, stack);
     }
 
 }
