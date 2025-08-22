f1bd74722b2ad7b7ecf7af9c6bddd1578652dbba
Xires87 <frycmod@gmail.com>
2025-08-22 12:12:17 +0200

    added missing translations

--- src/main/java/net/fryc/frycparry/config/FrycparryClientConfig.java
+++ src/main/java/net/fryc/frycparry/config/FrycparryClientConfig.java
@@ -10,26 +10,32 @@ import net.fryc.frycparry.util.ConfigHelper;
 public class FrycparryClientConfig implements ConfigData {
 
     @Comment("Negative values move icon left, and positive - right")
+    @ConfigEntry.Gui.Tooltip
     public int blockCooldownIconX = 0;
 
     @Comment("Negative values move icon up, and positive - down")
+    @ConfigEntry.Gui.Tooltip
     public int blockCooldownIconY = 23;
 
     @Comment("Possible values: CENTER, DOWN, RIGHT_DOWN, LEFT_DOWN")
+    @ConfigEntry.Gui.NoTooltip
     public ConfigHelper.HudIconPosition blockCooldownIconStartingPosition = ConfigHelper.HudIconPosition.CENTER;
 
     @Comment("When true, shows colored shield when blocking: \n" +
             "Red - block delay (you will take full hit); \n" +
             "Green - attack will be parried; \n" +
             "Yellow - attack will be blocked;")
+    @ConfigEntry.Gui.Tooltip
     public boolean showParryIndicator = false;
 
     public boolean showParryAttributesInTooltips = true;
 
     @Comment("When false, dontUseParryKey is toggleable (press it to disable blocking and parrying, and press again to enable)")
+    @ConfigEntry.Gui.NoTooltip
     public boolean holdDontUseParryKey = false;
 
     @Comment("Rotation, translation and scale of items when blocking")
+    @ConfigEntry.Gui.NoTooltip
     public boolean useOriginalRotation = false;
 
     @ConfigEntry.Gui.Tooltip
@@ -58,6 +64,7 @@ public class FrycparryClientConfig implements ConfigData {
     @Comment("If you play singleplayer, use server sided config (axe.json5, sword.json5 etc.) for options under this comment. " +
             "These options only disable keybind (game behaves like you have never pressed parry key if you have a disabled item in your mainhand)." +
             "You will still get cooldowns after swapping disabled items")
+    @ConfigEntry.Gui.NoTooltip
     public boolean enableBlockingWithSword = true;
     public boolean enableBlockingWithAxe = true;
     public boolean enableBlockingWithHoe = true;
